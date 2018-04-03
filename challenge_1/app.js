ff8337e8492bdb285e8cdf25a7729fda25356331
inh556 <yanbin.jin@gmail.com>
2018-04-02 19:57:07 -0700

    add game infos, like socres, rounds, players

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,7 +5,43 @@ var inputAvailable = true;
 var elements = document.getElementsByClassName("square");
 var firstPlayer = prompt("Input your name," , "");
 var secondPlayer = prompt("Input your name," , "");
-document.getElementById("players").innerHTML = firstPlayer + ' VS ' + secondPlayer;
+var firstMover = firstPlayer;
+var scores = {};
+scores[firstPlayer] = 0;
+scores[secondPlayer] = 0;
+var firstMover;
+var rounds = 1;
+var displayInfos = function() {
+	document.getElementById("players").innerHTML = firstPlayer + ' VS ' + secondPlayer;
+	document.getElementById("scores").innerHTML = scores[firstPlayer] + ' : ' + scores[secondPlayer];
+	document.getElementById("rounds").innerHTML = rounds;
+};
+displayInfos();
+// restart a new game
+var restart = function() {
+	for(var i = 0; i < elements.length; i++) {
+		elements[i].innerHTML = "";
+	}
+	records = Array(9).fill(null);
+	inputAvailable = true;
+	counter = 0;
+	rounds+=1;
+	displayInfos();
+};
+// find who is winner
+var findWinner = function() {
+	var winner = secondPlayer;	
+	if(firstMover === secondPlayer) {
+		winner = firstPlayer;
+	}
+	if(counter % 2) {
+			winner = firstPlayer;
+	}
+	scores[winner] += 1;
+	firstMover = winner;
+	document.getElementById("warning").innerHTML = winner + ' win!';
+};
+// evaluate if win
 var winOrNot = function() {
 	var possibleResults = [
 		[0, 1, 2],
@@ -17,41 +53,29 @@ var winOrNot = function() {
 		[0, 4, 8],
 		[2, 4, 6]
 	];
+	// check if win 
 	for(var i = 0; i < possibleResults.length; i++) {
 		[a, b, c] = possibleResults[i];
 		if(records[a] && records[a] === records[b] && records[a] === records[c]) {
-			var winner = secondPlayer;
-			if(counter % 2) {
-				console.log("working");
-				winner = firstPlayer;
-			}
-			console.log(counter);
-			document.getElementById("warning").innerHTML = winner + ' win!';
+			findWinner(counter);		
 			inputAvailable = false;
 		}
 	}
+	// if no one win, and the last step of one round, evalute to tied game;
 	if(counter === 9 && inputAvailable === true) {
-		console.log('it is working!')
 		inputAvailable = false;
 		document.getElementById('warning').innerHTML = "Game Over, please restart a new game!";
 	}
 };
 
-var restart = function() {
-	console.log('passed in');
-	for(var i = 0; i < elements.length; i++) {
-		elements[i].innerHTML = "";
-		records = Array(9).fill(null);
-		inputAvailable = true;
-		counter = 0;
-	}
-}
 
+// set listener for each square
 for(var i = 0; i < elements.length; i++) {
 	elements[i].addEventListener("click", function() {
 	if(!inputAvailable) {
 		document.getElementById("warning").innerHTML = "Game over! Please restart a new game!";
 	} else {
+			// record each mark in the board
 			if(records[Number(this.id)]) {
 			document.getElementById("warning").innerHTML = "You are not allowed to put here!";
 		} else {
@@ -68,4 +92,3 @@ for(var i = 0; i < elements.length; i++) {
 	}
 	});
 }
-
