bbbf7b9c98d3861ba4ae69a5939dee0ff672194a
inh556 <yanbin.jin@gmail.com>
2018-04-24 15:19:09 -0700

    set up server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan')
 
+const PORT = 3005;
+const app =  express();
 
+app.use(bodyParser.json());
+app.use(logger('dev'))
+app.use(express.static('./client'));
+
+app.get('/', function(req, res) {
+  res.send('Hellp world!')
+})
+
+app.listen(PORT, function(){
+  console.log(`App is listening on ${PORT}`);
+})
