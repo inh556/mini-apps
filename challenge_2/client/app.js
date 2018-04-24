9cd2b519cd1a45bfbef0295a344e293493529778
inh556 <yanbin.jin@gmail.com>
2018-04-24 15:20:00 -0700

    connect to server, make listner work

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,12 @@
 
-
+$(document).ready(function() {
+  app.init();
+});
+const app = {
+  server: 'hppt://127.0.0.1:3000',
+  init: function(){
+    $(document).on('click','#submit', function(){
+      alert('listener is working!');
+    })
+  },
+}
