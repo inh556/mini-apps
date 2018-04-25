32ac47338ae896a2d08b60e1f83e79547cb119ce
inh556 <yanbin.jin@gmail.com>
2018-04-25 15:50:32 -0700

    making data render to screen correctly

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -11,24 +11,40 @@ const app = {
       event.preventDefault();
       // get user's input info
       var data = $('#inputText').val();  
+      $('#inputText').val("")
       // call handld function 
-      app.insertToFile(data);
+      app.convertToCsv(data);
     })
   },
-  insertToFile: function(data) {
+  convertToCsv: function(data) {
     // with post method, send data to server 
-    var test = {'info': data};
+    var data = {'info': data};
     $.ajax({
       url: '/',
       contentType: 'application/json',
       method: "POST",      
-      data: JSON.stringify(test),
-    }).done(function(res){
-      console.log(res);
+      data: JSON.stringify(data),
+    })
+    .done(function(res){
+      app.renderDataToScreen(res);
     })
   },
-  renderDataToScreen: function() {
-    // get method fetch data from server side
-    
+  renderDataToScreen: function(res) {
+    const contents = res.split('\n');
+    const header = contents.splice(0, 1)[0].split(',');
+    if(!$('#insertHeader > th').length) {
+      header.forEach(function(el) {
+        $('#insertHeader').append(`<th>${el}</th>`)
+      })
+    }
+    contents.forEach(function(row) {
+      // insert row
+      $("#insertContents").append(`<tr>${
+        // insert item for row
+        row.split(',').forEach((item) => {
+          $('#insertContents').append(`<td>${item}</td>`);
+        })
+      }</tr>`)
+    })
   }
 }
