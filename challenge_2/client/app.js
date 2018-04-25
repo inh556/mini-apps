c669dd0e69cfe1a598d2ea3fafc73d81b185d8b7
inh556 <yanbin.jin@gmail.com>
2018-04-24 17:00:26 -0700

    add post method

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -3,10 +3,32 @@ $(document).ready(function() {
   app.init();
 });
 const app = {
-  server: 'hppt://127.0.0.1:3000',
+  server: 'hppt://127.0.0.1:3005',
+  // init when page load
   init: function(){
-    $(document).on('click','#submit', function(){
-      alert('listener is working!');
+    // add listner to submit button
+    $('#submit').on('click', function(event){
+      event.preventDefault();
+      // get user's input info
+      var data = $('#inputText').val();  
+      // call handld function 
+      app.insertToFile(data);
     })
   },
+  insertToFile: function(data) {
+    // with post method, send data to server 
+    alert('insert function working!')
+    var test = {'info': data};
+    $.ajax({
+      url: '/',
+      contentType: 'application/json',
+      method: "POST",      
+      data: JSON.stringify(test),
+    }).done(function(res){
+    })
+  },
+  renderDataToScreen: function() {
+    // get method fetch data from server side
+    
+  }
 }
