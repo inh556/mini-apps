36372b6399f2e317333f26819aa6d3b37984a392
inh556 <yanbin.jin@gmail.com>
2018-04-24 19:52:35 -0700

    finished post method, send csv back to client

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan')
+const fs = require('fs');
 
 const PORT = 3005;
 const app =  express();
@@ -17,8 +18,39 @@ app.post('/', function(req, res) {
   // for post method
   const data = JSON.parse(req.body.info);
   // save to file
-  res.send(data);
+  formToCsv(data, function(result) {
+    res.send(result);
+  })
 })
+
+function formToCsv(data, callback) {
+  let tHeader = [];
+  let contents = [];
+  const keys = Object.keys(data);
+  for(let key of keys) {
+    if(key !== 'children') {
+      tHeader.push(key);
+    }
+  }
+  const header = tHeader.join(',');
+  contents.push(header);
+  function handleContent(data) {
+    let content = [];
+    tHeader.forEach(function(el){
+      content.push(data[el]);
+    })
+    contents.push(content.join(','));
+    if(data.children) {
+      data.children.forEach(function(child) {
+        handleContent(child)
+      });
+    }
+  }
+  handleContent(data);
+  //const content = contents.join('\n');
+  const response = contents.join('\n');
+  callback(response);
+}
 app.listen(PORT, function(){
   console.log(`App is listening on ${PORT}`);
 })
