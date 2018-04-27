3b0e96f6c9a7ce32139b917864d5d206006d8180
inh556 <yanbin.jin@gmail.com>
2018-04-26 17:20:18 -0700

    set up server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = 3003;
+app.use(express.static(path.join(__dirname, './public')));
 
+app.use(bodyParser.json());
 
+// app.get('/client/app.js', (req, res) => {
+//   //get
+//   res.end();
+// });
+
+// app.post('/', (req, res) => {
+//   //post
+// })
+
+app.listen(PORT, () => {
+  console.log(`App listening one ${PORT}`);
+})
