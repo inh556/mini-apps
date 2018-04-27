bea1ba7ab67e200ecfbdf3a008e78996904516a8
inh556 <yanbin.jin@gmail.com>
2018-04-26 17:24:34 -0700

    add game board and add listener for each square

--- challenge_3/public/app.js
+++ challenge_3/public/app.js
@@ -22,4 +22,22 @@ $("#inputArea").submit(function(event) {
 	//$("#firstPlayer").text = '';
 	document.getElementById("firstPlayer").value = "";
 	document.getElementById("secondPlayer").value = "";
-})
+});
+
+for(var i = 0; i  < board.length; i++) {
+	var $newRow = $(`<div class="rows" id= ${i}></div>`);
+	$("#board").append($newRow);
+	for(var j = 0; j < board[i].length; j++) {
+		var row = i;
+		var newSquare = $(`<div class="squares" id=` + row +'' + j +  `></div>`);
+		$("#"+i).append(newSquare);
+	}
+}
+
+// add a listner for each square
+var elements = document.getElementsByClassName("squares");
+for(var i = 0; i< elements.length; i++) {
+	elements[i].addEventListener("click", function() {
+		placeMark(this.id);
+	})
+}
